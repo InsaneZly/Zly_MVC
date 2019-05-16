404fe97f1d7177660c949d7528384bc58162d88b
Insane_Killah <597782267@qq.com>
2019-05-16 16:33:37 +0800

    添加项目文件。

---
+++ WebApplicationZly/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationZly.MvcApplication" Language="C#" %>
---
+++ WebApplicationZly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationZly.Startup))]
+namespace WebApplicationZly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
